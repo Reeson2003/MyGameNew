a9d084706a97ba4f61607080fb6d39ff2a56ca43
nimtego <nimtego@mail.ru>
2016-12-07 22:39:28 +0300

    ExperienceTest

---
+++ TestMyGameNew/ru/reeson2003/model/characters/creatures/ExperienceTest.java
@@ -0,0 +1,46 @@
+package ru.reeson2003.model.characters.creatures;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by nimtego_loc on 07.12.2016.
+ */
+public class ExperienceTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void addExperience() throws Exception {
+
+    }
+
+    @Test
+    public void getLevel() throws Exception {
+
+    }
+
+    @Test
+    public void getExperience() throws Exception {
+
+    }
+
+    @Test
+    public void getExpToNextLevel() throws Exception {
+
+    }
+
+    @Test
+    public void getSkillPoints() throws Exception {
+
+    }
+
+}
