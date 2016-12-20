5e105c1c9f439a07a4ad5fd6ddcce8864b60a331
reeson2003 <reeson2003@gmail.com>
2016-12-21 00:08:45 +0300

    Main5 updated: ДаниэльДефо бьет сразу 4 моба

--- src/ru/reeson2003/model/characters/creatures/NonPlayerCharacter/MonsterFactoryTestImpl.java
+++ src/ru/reeson2003/model/characters/creatures/NonPlayerCharacter/MonsterFactoryTestImpl.java
@@ -7,11 +7,20 @@ import ru.reeson2003.model.characters.creatures.ParametersController;
  */
 public class MonsterFactoryTestImpl implements MonsterFactory {
     private MonsterParametersManager manager;
+
     public MonsterFactoryTestImpl() {
         manager = MonsterParametersManager.getInstance();
     }
+
     @Override
     public Monster getMonster(int monsterID) {
-        return new Monster("Ant Qeen", ParametersController.getMonsterParametersController(monsterID));
+        if (monsterID == 1)
+            return new Monster("Ant Qeen", ParametersController.getMonsterParametersController(monsterID));
+        else if (monsterID == 2)
+            return new Monster("Skeleton Raider", ParametersController.getMonsterParametersController(monsterID));
+        else if (monsterID == 3)
+            return new Monster("Goblin scout", ParametersController.getMonsterParametersController(monsterID));
+        else
+            return new Monster("Elpy", ParametersController.getMonsterParametersController(monsterID));
     }
 }
