9650f0d1594598fa3c5a1e9bb02d77c52e43490d
Nimtego <nimtego@mail.ru>
2016-12-27 12:12:47 +0300

    Update EasyHealAbility.java
    Test Heal

--- src/ru/reeson2003/model/characters/battle/abilities/EasyHealAbility.java
+++ src/ru/reeson2003/model/characters/battle/abilities/EasyHealAbility.java
@@ -10,24 +10,29 @@ import ru.reeson2003.model.characters.creatures.Creature;
 public class EasyHealAbility extends Ability {
 
     private int healSize;
+    private int manaCost;
 
     public EasyHealAbility(Creature owner) {
         this.name = "Easy Heal";
         this.information = "Value of heal";
         this.owner = owner;
         this.msg = new EasyHealMsg(this.owner.getAddress());
-        this.healSize = /*AbilityConstant.EASY_HEAL_COEFF*/ 10 + (owner.getIntellect() / 2);
-        this.coolDownAbility = /*AbilityConstant.EASY_HEAL_COOLDOWN*/ new CoolDownAbility(10000 / owner.getWisdom());  // 1 - один хит персеконд (спид атак 1 по умолчанию думаю надо дабл делать)
+        this.healSize = AbilityConstant.EASY_HEAL_COEFF + (owner.getIntellect() / 2);
+        this.manaCost = AbilityConstant.EASY_HEAL_MANACOST;
+        this.coolDownAbility = new CoolDownAbility(AbilityConstant.EASY_HEAL_COOLDOWN / (owner.getWisdom() / 10));  // 1 - один хит персеконд (спид атак 1 по умолчанию думаю надо дабл делать)
     }
     @Override
     public void use(Creature to) {
         if(to != null)
             msg.setTo(to.getAddress());
-        if(coolDownAbility.getIsActive()) {
+        else
+            msg.setTo(owner.getAddress());
+        if(coolDownAbility.getIsActive() && owner.getMana() - manaCost > 0) {
             coolDownAbility.use();
             msg.exec();
         }
     }
+    
     @Override
     public String getInformation() {
         return  "" +information +" - " +healSize;
