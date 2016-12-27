bf23b07ced49317e8e486b52468db75e386ac3ec
Nimtego <nimtego@mail.ru>
2016-12-27 11:24:24 +0300

    AbilityConstants
    AbilityConstants

---
+++ src/ru/reeson2003/model/characters/battle/abilities/AbilityConstants.java
@@ -0,0 +1,13 @@
+package ru.reeson2003.model.characters.battle.abilities;
+
+/**
+ * Created by nimtego_loc 27.12.16.
+ */
+public class AbilityConstants {
+    //Heal
+    public static final int EASY_HEAL_COEFF = 10;
+    public static final int EASY_HEAL_COOLDOWN = 10000;
+    //Fury
+    public static final int FURY_HIT_COEFF = 30;
+    public static final int FURY_HIT_COOLDOWN = 6000;
+}
