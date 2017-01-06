81f80e35102fb057ddcb44d1040d19531b8fb7f3
nimtego <nimtego@mail.ru>
2017-01-07 00:33:30 +0300

    чутку шаблонов тэстов

---
+++ TestMyGameNew/ru/reeson2003/model/characters/battle/abilities/EasyHealMsgTest.java
@@ -0,0 +1,19 @@
+package ru.reeson2003.model.characters.battle.abilities;
+
+import org.junit.Test;
+
+/**
+ * Created by nimtego_loc on 07.01.2017.
+ */
+public class EasyHealMsgTest {
+    @Test
+    public void getEasyHealMsg() throws Exception {
+
+    }
+
+    @Test
+    public void exec() throws Exception {
+
+    }
+
+}
---
+++ TestMyGameNew/ru/reeson2003/model/characters/battle/abilities/FuryHitAbilityTest.java
@@ -0,0 +1,19 @@
+package ru.reeson2003.model.characters.battle.abilities;
+
+import org.junit.Test;
+
+/**
+ * Created by nimtego_loc on 07.01.2017.
+ */
+public class FuryHitAbilityTest {
+    @Test
+    public void use() throws Exception {
+
+    }
+
+    @Test
+    public void getDamageAbility() throws Exception {
+
+    }
+
+}
